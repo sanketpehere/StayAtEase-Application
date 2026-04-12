99997ab10c1c793d69e7ab1935eae08493d6e835
WebDevilSanket <105560181+WebDevilSanket@users.noreply.github.com>
2026-04-12 16:59:55 +0530

    Edited _redirects

--- app-ui/StayAtEase/vite.config.js
+++ app-ui/StayAtEase/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // ← add this
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
     proxy: {
